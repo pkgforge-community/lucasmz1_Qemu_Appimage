c1dbd7e0f6bdca65855c708c9e057aff645afe9c
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-07-30 16:48:53 -0300

    Update paths for libglycin files in qemu-universal.sh

--- qemu-universal.sh
+++ qemu-universal.sh
@@ -13,10 +13,10 @@ cp ${GITHUB_WORKSPACE}/files/pacman.conf -t ${GITHUB_WORKSPACE}/arch/root.x86_64
 cd ${GITHUB_WORKSPACE}
 sudo chroot ./arch/root.x86_64/ /usr/bin/bash -c "pacman -Syyu --noconfirm && pacman -S qemu-full jack2 --noconfirm && rm -rf /var/cache/pacman/pkg/* && exit"
 cp ${GITHUB_WORKSPACE}/files/AppRun ${GITHUB_WORKSPACE}/arch/
-sudo cp ${GITHUB_WORKSPACE}/files/libglycin_2.so ${GITHUB_WORKSPACE}/arch/usr/lib/
-sudo cp ${GITHUB_WORKSPACE}/files/libglycin_ng.so ${GITHUB_WORKSPACE}/arch/usr/lib/
-sudo mv ${GITHUB_WORKSPACE}/arch/usr/lib/libglycin_ng.so ${GITHUB_WORKSPACE}/arch/usr/lib/libglycin-2.so
-sudo ln -frs ${GITHUB_WORKSPACE}/arch/usr/lib/libglycin-2.so ${GITHUB_WORKSPACE}/arch/usr/lib/libglycin-2.so.0
+sudo cp ${GITHUB_WORKSPACE}/files/libglycin_2.so ${GITHUB_WORKSPACE}/arch/root/usr/lib/
+sudo cp ${GITHUB_WORKSPACE}/files/libglycin_ng.so ${GITHUB_WORKSPACE}/arch/root/usr/lib/
+sudo mv ${GITHUB_WORKSPACE}/arch/root/usr/lib/libglycin_2.so ${GITHUB_WORKSPACE}/arch/root/usr/lib/libglycin-2.so
+sudo ln -frs ${GITHUB_WORKSPACE}/arch/root/usr/lib/libglycin-2.so ${GITHUB_WORKSPACE}/arch/root/usr/lib/libglycin-2.so.0
 chmod a+x ${GITHUB_WORKSPACE}/arch/AppRun
 cp ${GITHUB_WORKSPACE}/files/qemu.svg -t ${GITHUB_WORKSPACE}/arch/
 cp ${GITHUB_WORKSPACE}/files/qemu.desktop -t ${GITHUB_WORKSPACE}/arch/
